e4049f3075e3b4af4255d5afc78643c60bca972e
NGUYEN_CHIEN <nguyenchien.0790@gmail.com>
2022-10-24 22:45:20 +0700

    lan 1

---
+++ src/main/java/rikkei/academy/model/FormCustomer.java
@@ -0,0 +1,52 @@
+package rikkei.academy.model;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FormCustomer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private MultipartFile avatar;
+
+    public FormCustomer() {
+    }
+
+    public FormCustomer(Long id, String firstName, String lastName, MultipartFile avatar) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.avatar = avatar;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public MultipartFile getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(MultipartFile avatar) {
+        this.avatar = avatar;
+    }
+}
---
+++ src/main/java/rikkei/academy/service/customer/ICustomerService.java
@@ -0,0 +1,8 @@
+package rikkei.academy.service.customer;
+
+import rikkei.academy.model.Customer;
+import rikkei.academy.service.IGenericService;
+
+public interface ICustomerService extends IGenericService<Customer> {
+}
+
